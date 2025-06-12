a3445d480a0388f53f9ea0e90569140173d47b48
EllyCarlos <ellycarlos7915@gmail.com>
2025-06-12 18:43:48 +0300

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const axiosi = axios.create({
+const axiosInstance = axios.create({
     withCredentials: true,
     baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:8000'
 });
 
-export default axios;
+export default axiosInstance;
